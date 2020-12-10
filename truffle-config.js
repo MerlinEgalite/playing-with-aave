61416b7913a71a4263621899d8ceee500c4b69a9
MerlinEgalite <egalite.merlin@gmail.com>
2020-12-10 13:09:28 +0100

    remove broken contract and add new one

--- truffle-config.js
+++ truffle-config.js
@@ -2,7 +2,6 @@ const HDWalletProvider = require("@truffle/hdwallet-provider")
 const dotenv = require('dotenv')
 dotenv.config()
 
-
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID
 
@@ -14,17 +13,10 @@ module.exports = {
       network_id: '*'
     },
     kovan: {
-      provider: () => new HDWalletProvider(PRIVATE_KEY, `https://ropsten.infura.io/v3/${INFURA_PROJECT_ID}`),
-      network_id: 3
+      provider: () => new HDWalletProvider(PRIVATE_KEY, `https://kovan.infura.io/v3/${INFURA_PROJECT_ID}`),
+      network_id: 42
     }
   },
-
-  // Set default mocha options here, use special reporters etc.
-  mocha: {
-    // timeout: 100000
-  },
-
-  // Configure your compilers
   compilers: {
     solc: {
       version: "0.6.12",
