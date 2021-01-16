7c0abba60f665c8f54378937e1dea414f4223ef9
MerlinEgalite <egalite.merlin@gmail.com>
2021-01-16 16:29:21 +0100

    change architecture

--- web-app/contracts/addresses.ts
+++
@@ -1,7 +0,0 @@
-const addresses = {
-	creditDelegation: '0x93a284C91768F3010D52cD37f84f22c5052be40b',
-	dai: '0xff795577d9ac8bd7d90ee22b6c1703490b6512fd',
-	lendingPool: '0xE0fBa4Fc209b4948668006B2bE61711b7f465bAe',
-}
-
-export default addresses
