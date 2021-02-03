715531d2166fb15809f5fe4ffbe0d5d4b2b12230
MerlinEgalite <egalite.merlin@gmail.com>
2021-02-03 12:06:00 +0100

    add abi CVCD

--- contracts/addresses.ts
+++ contracts/addresses.ts
@@ -1,6 +1,7 @@
 const addresses = {
 	simpleCreditDelegation: '0x0aF1B3F6e2B7512ae8E6ad5Ae415D18E1919A0FE',
 	governanceCreditDelegation: '0x1e32Ac68638747D2eFA7Fa8F16207D93cF1CFB8a',
+	convictionVotingCreditDelegation: '0x0',
 	dai: '0xff795577d9ac8bd7d90ee22b6c1703490b6512fd',
 	lendingPool: '0xE0fBa4Fc209b4948668006B2bE61711b7f465bAe',
 }
